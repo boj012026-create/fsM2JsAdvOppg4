f2009c8e9671d3f31c4a293c51e5d67837cf43f6
Jan Olav <boj012026@gmail.com>
2026-04-27 11:10:40 +0200

    created a template for project

--- src/main.js
+++ src/main.js
@@ -1,25 +1,21 @@
 import './style.css'
-import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
 import {CustomButton} from "./components/custom-button.js"
 
 document.querySelector('#app').innerHTML = `
 <section id="center">
   <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
+    <img id="hero-img" src="${viteLogo}"   logo" />
   </div>
   <div>
-    <h1 id="page-title">Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
+    <h1>Project Title</h1>
+    <p id="info-p">This is where information get sent</p>
   </div>
-  <button id="counter" type="button" class="counter"></button>
+  <div id="button-container">
+    <button id="counter" type="button" class="counter"></button>
+  </dvi>
 </section>
-
-
 `
 
 setupCounter(document.querySelector('#counter'))
