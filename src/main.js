86c03ea4d549971523337a06ffdacc3643ba76ad
Jan Olav <boj012026@gmail.com>
2026-04-28 08:40:06 +0200

    removed unused btns

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import './style.css'
 import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.js'
-import {CustomButton} from "./components/custom-button.js"
 import { adviceBtn } from "./components/advice-btn.js"
 
 document.querySelector('#app').innerHTML = `
@@ -14,15 +12,10 @@ document.querySelector('#app').innerHTML = `
     <p id="info-p">This is where information get sent</p>
   </div>
   <div id="button-container">
-    <button id="counter" type="button" class="counter"></button>
   </dvi>
 </section>
 `
 
-setupCounter(document.querySelector('#counter'))
 
 document.getElementById('button-container')
   .append(adviceBtn(document.getElementById('info-p')));
-document
-  .querySelector(".hero")
-  .appendChild(CustomButton("click me!", "After click", 3000));
